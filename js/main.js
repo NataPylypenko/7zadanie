e7b0010afdf6d1908530d10695ce8c8543dbfb93
NataPylypenko <npylypenko.work@gmail.com>
2019-12-17 16:31:44 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let start = document.getElementById("start"),
+    budgetValue = document.getElementsByClassName('budget-value'),
+    daybudgetValue =document.getElementsByClassName('daybudget-value'),
+    expensesValue=document.getElementsByClassName('expenses-value');
+    optionalexpensesValue=document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue=document.getElementsByClassName('income-value'),
+    monthsavingsValue=document.getElementsByClassName('monthsavings-value'),
+    yearsavingsValue=document.getElementsByClassName('yearsavings-value'),
+    yearValue=document.getElementsByClassName('year-value'),
+    monthValue=document.getElementsByClassName('month-value'),
+    dayValue=document.getElementsByClassName('day-value'),
+    expensesItem=document.getElementsByClassName('expenses-item'),
+    BTN=document.getElementsByTagName('button'),
+    obRasBTN=BTN[0],
+    neObRasBTN=BTN[1],
+    rasDayBudBTN=BTN[2],
+    chooseInc=document.querySelector('.choose-income'),
+    chooseSum=document.querySelector('.choose-sum'),
+    choosePercent=document.querySelector('choose-percent'),
+    year=document.querySelector('year-value'),
+    month=document.querySelector('month-value'),
+    day=document.querySelector('day-value');
+
+
+
+
+
+
+    console.log(chooseSum);
+
+    
